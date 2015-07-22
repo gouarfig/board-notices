1494abe81dd943e27f09bcbae5d5fa92adeb1414
FredQ <gouarfig@yahoo.fr>
2015-07-22 13:02:47 +0100

    Changed structure

---
+++ fq/boardnotices/migrations/create_tables.php
@@ -0,0 +1,61 @@
+<?php
+/**
+*
+* Board Notices Manager
+*
+* @version 1.0.0
+* @copyright (c) 2015 Fred Quointeau
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace fq\boardnotices\migrations;
+
+class create_tables extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return $this->db_tools->sql_table_exists($this->table_prefix . 'notices');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'add_tables' => array(
+				$this->table_prefix . 'notices' => array(
+					'COLUMNS'		=> array(
+						'notice_id'			=> array('UINT', null, 'auto_increment'),
+						'title'				=> array('VCHAR:50', ''),
+						'message'			=> array('MTEXT_UNI', ''),
+						'message_uid'		=> array('VCHAR:8', ''),
+						'message_bitfield'	=> array('VCHAR:255', ''),
+						'message_options'	=> array('UINT', 0),
+						'message_bgcolor'	=> array('VCHAR:6', ''),
+						'active'			=> array('UINT:1', 0),
+						'persistent'		=> array('UINT:1', 1),
+						'last'				=> array('UINT:1', 0),
+						'left_id'			=> array('UINT', 1),
+						'right_id'			=> array('UINT', 2),
+					),
+					'PRIMARY_KEY'	=> 'notice_id',
+					'KEYS'		=> array(
+						'active_index'		=> array('INDEX', 'active'),
+						'lid'				=> array('INDEX', 'left_id'),
+					),
+				),
+				$this->table_prefix . 'notices_rules' => array(
+					'COLUMNS'		=> array(
+						'notice_rule_id'	=> array('UINT', null, 'auto_increment'),
+						'notice_id'			=> array('UINT', 0),
+						'rule'				=> array('VCHAR:50', ''),
+						'conditions'		=> array('MTEXT_UNI', ''),
+					),
+					'PRIMARY_KEY'	=> 'notice_rule_id',
+					'KEYS'		=> array(
+						'nid'		=> array('INDEX', 'notice_id'),
+					),
+				),
+			)
+		);
+	}
+}
---
+++ fq/boardnotices/rules/birthday.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*
+* Board Notices Manager
+*
+* @version 1.0.0
+* @copyright (c) 2015 Fred Quointeau
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace fq\boardnotices\rules;
+
+class birthday implements rule
+{
+	private $user;
+	
+	public function __construct(\phpbb\user $user) {
+		$this->user = $user;
+	}
+	
+	public function isTrue($conditions) {
+		$valid = false;
+		return $valid;
+	}
+}
