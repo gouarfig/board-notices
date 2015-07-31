761122c4090f9a5ea6b515c5f7ec9d5402204e86
Mister Queue <gouarfig@yahoo.fr>
2015-07-31 23:52:30 +0000

    Test with phpunit

---
+++ tests/travis_test.php
@@ -0,0 +1,10 @@
+<?php
+class TravisTest extends PHPUnit_Framework_TestCase
+{
+    public function testTravis()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+    }
+}
+?>
