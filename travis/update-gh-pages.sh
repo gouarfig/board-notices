2fe42abfe985e8ffb2f429e5bd61ce8fa9e7d66f
Gouarfig <gouarfig@yahoo.fr>
2015-08-06 21:49:32 +0100

    Try to add some test coverage information

---
+++ travis/update-gh-pages.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp -R logs/coverage $HOME/coverage
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/gouarfig/gouarfig.github.io.git  gh-pages > /dev/null
+
+  #go into directory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/coverage/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with coverage\n"
+fi
