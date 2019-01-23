e256629c460dcd85242dbf51e279d9bd7ab177b1
FredQ <gouarfig@yahoo.fr>
2019-01-23 16:25:56 +0000

    Extract javascript from template to JS

--- adm/style/boardnotices.js
+++ adm/style/boardnotices.js
@@ -1,4 +1,4 @@
-(function($) {
+(function($, boardNoticeBgColor, boardNoticeDefaultBgColor) {
 
 	'use strict';
 
@@ -39,6 +39,42 @@
 				rowId.removeClass('active-rule').addClass('inactive-rule');
 			}
 		});
+
+		function getColpickConfig(colorpick) {
+			return {
+				layout: 'hex',
+				submit: 0,
+				onBeforeShow: function() {
+					if (colorpick !== '') {
+						$(this).colpickSetColor(colorpick);
+						colorpick = '';
+					}
+				},
+				onChange: function(hsb, hex, rgb, el, bySetColor) {
+					$(el).css({
+						'border-right-color': '#' + hex,
+						'border-right-width': '20px',
+						'border-right-type': 'solid'
+					});
+					if (!bySetColor) {
+						$(el).val(hex);
+					}
+				}
+			}
+		}
+
+		$('#board_notice_bgcolor').colpick(
+			getColpickConfig(boardNoticeBgColor)
+		).keyup(function() {
+			$(this).colpickSetColor(this.value || 'ffffff');
+		});
+
+		$('#board_notice_default_bgcolor').colpick(
+			getColpickConfig(boardNoticeDefaultBgColor)
+		).keyup(function() {
+			$(this).colpickSetColor(this.value || 'ffffff');
+		});
+
 	});
 
-})(jQuery);
+})(jQuery, boardNoticeBgColor, boardNoticeDefaultBgColor);
