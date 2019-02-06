0b3e0f792f085e6341c42e4303faae6755f65058
FredQ <gouarfig@yahoo.fr>
2019-02-06 16:52:37 +0000

    Remove fecking functional tests

--- tests/functions.php
+++ tests/functions.php
@@ -1,9 +1,6 @@
 <?php
 
-if (!function_exists('get_group_name'))
+function get_group_name($group_id)
 {
-	function get_group_name($group_id)
-	{
-		return "Group Name";
-	}
+	return "Group Name";
 }
