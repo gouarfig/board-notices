d49cd834c568bf7774f606bbf5d743afa5adc816
FredQ <gouarfig@yahoo.fr>
2019-02-08 16:36:35 +0000

    Sample tests for listener class

--- tests/listener_test.php
+++ tests/listener_test.php
@@ -3,6 +3,7 @@
 namespace fq\boardnotices\tests;
 
 use fq\boardnotices\event\listener;
+use fq\boardnotices\service\constants;
 
 class listener_test extends \PHPUnit_Framework_TestCase
 {
@@ -19,7 +20,7 @@ class listener_test extends \PHPUnit_Framework_TestCase
 		$user = $this->getMockBuilder('\phpbb\user')->disableOriginalConstructor()->getMock();
 
 		/** @var \phpbb\config\config $config */
-		$config = $this->getMockBuilder('\phpbb\config\config')->disableOriginalConstructor()->getMock();
+		$config = new \phpbb\config\config(array());
 
 		/** @var \phpbb\template\template $template */
 		$template = $this->getMockBuilder('\phpbb\template\template')->disableOriginalConstructor()->getMock();
@@ -41,5 +42,124 @@ class listener_test extends \PHPUnit_Framework_TestCase
 
 		$listener = new listener($user, $config, $template, $request, $controller_helper, $language, $notices_repository, $notices_seen_repository);
 		$this->assertNotNull($listener);
+	}
+
+	/**
+	 * @depends testCanInstantiate
+	 */
+	function testExtensionNotEnabled()
+	{
+		/** @var \phpbb\user $user */
+		$user = $this->getMockBuilder('\phpbb\user')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\config\config $config */
+		$config = new \phpbb\config\config(array(
+			constants::$CONFIG_ENABLED => false,
+		));
+
+		/** @var \phpbb\template\template $template */
+		$template = $this->getMockBuilder('\phpbb\template\template')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\request\request_interface $request */
+		$request = new \phpbb_mock_request();
+
+		/** @var \phpbb\controller\helper $controller_helper */
+		$controller_helper = $this->getMockBuilder('\phpbb\controller\helper')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\language\language $language */
+		$language = $this->getMockBuilder('\phpbb\language\language')->disableOriginalConstructor()->getMock();
+
+		/** @var \fq\boardnotices\repository\notices_interface $notices_repository */
+		$notices_repository = $this->getMockBuilder('\fq\boardnotices\repository\notices_interface')->getMock();
+
+		/** @var \fq\boardnotices\repository\notices_seen_interface $notices_seen_repository */
+		$notices_seen_repository = $this->getMockBuilder('\fq\boardnotices\repository\notices_seen_interface')->getMock();
+
+		$listener = new listener($user, $config, $template, $request, $controller_helper, $language, $notices_repository, $notices_seen_repository);
+		$listener->display_board_notices();
+	}
+
+
+	/**
+	 * @depends testCanInstantiate
+	 */
+	function testPreviewWithWrongKey()
+	{
+		/** @var \phpbb\user $user */
+		$user = $this->getMockBuilder('\phpbb\user')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\config\config $config */
+		$config = new \phpbb\config\config(array(
+			constants::$CONFIG_ENABLED => false,
+			constants::$CONFIG_PREVIEW_KEY => '123',
+		));
+
+		/** @var \phpbb\template\template $template */
+		$template = $this->getMockBuilder('\phpbb\template\template')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\request\request_interface $request */
+		$request = new \phpbb_mock_request(array('bnid' => 11, 'bnpk' => '456'));
+
+		/** @var \phpbb\controller\helper $controller_helper */
+		$controller_helper = $this->getMockBuilder('\phpbb\controller\helper')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\language\language $language */
+		$language = $this->getMockBuilder('\phpbb\language\language')->disableOriginalConstructor()->getMock();
+
+		/** @var \fq\boardnotices\repository\notices_interface $notices_repository */
+		$notices_repository = $this->getMockBuilder('\fq\boardnotices\repository\notices_interface')->getMock();
+
+		/** @var \fq\boardnotices\repository\notices_seen_interface $notices_seen_repository */
+		$notices_seen_repository = $this->getMockBuilder('\fq\boardnotices\repository\notices_seen_interface')->getMock();
+
+		$listener = new listener($user, $config, $template, $request, $controller_helper, $language, $notices_repository, $notices_seen_repository);
+		$listener->display_board_notices();
+	}
+
+	/**
+	 * @depends testCanInstantiate
+	 */
+	function testPreview()
+	{
+		global $phpbb_dispatcher;
+		$phpbb_dispatcher = new \phpbb_mock_event_dispatcher();
+
+		global $cache;
+		$cache = $this->getMockBuilder('\phpbb\cache\service')->disableOriginalConstructor()->getMock();
+
+		global $user;
+		/** @var \phpbb\user $user */
+		$user = $this->getMockBuilder('\phpbb\user')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\config\config $config */
+		$config = new \phpbb\config\config(array(
+			constants::$CONFIG_ENABLED => false,
+			constants::$CONFIG_PREVIEW_KEY => 'pkey',
+		));
+
+		/** @var \phpbb\template\template $template */
+		$template = $this->getMockBuilder('\phpbb\template\template')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\request\request_interface $request */
+		$request = new \phpbb_mock_request(array('bnid' => 11, 'bnpk' => 'pkey'));
+
+		/** @var \phpbb\controller\helper $controller_helper */
+		$controller_helper = $this->getMockBuilder('\phpbb\controller\helper')->disableOriginalConstructor()->getMock();
+
+		/** @var \phpbb\language\language $language */
+		$language = $this->getMockBuilder('\phpbb\language\language')->disableOriginalConstructor()->getMock();
+
+		/** @var \fq\boardnotices\repository\notices_interface $notices_repository */
+		$notices_repository = $this->getMockBuilder('\fq\boardnotices\repository\notices_interface')->getMock();
+
+		/** @var \fq\boardnotices\repository\notices_seen_interface $notices_seen_repository */
+		$notices_seen_repository = $this->getMockBuilder('\fq\boardnotices\repository\notices_seen_interface')->getMock();
+
+		$listener = new listener($user, $config, $template, $request, $controller_helper, $language, $notices_repository, $notices_seen_repository);
+		$listener->display_board_notices();
+
+		unset($cache);
+		unset($phpbb_dispatcher);
+		unset($user);
 	}
 }
